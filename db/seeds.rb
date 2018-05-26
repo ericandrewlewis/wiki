2f2ce242452f3ce92af72a471251c3312cb7d589
Eric Lewis <eric.andrew.lewis@gmail.com>
2018-05-26 18:56:23 -0400

    Add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+
+User.create!({
+  username: "eric",
+  password_digest: "$2a$04$UW/nNNbmO31M6HPzfEMqcOQH05z1dz.3VzehOSZKSUJW.KCG/IzCu",
+  email: "eric.andrew.lewis@gmail.com",
+  name: "Eric Lewis"
+})
